02de0286c9f7c55a087a97b1b44a6366e6b753b7
Svitlana Horodylova <horodylova.sv@gmail.com>
2024-08-05 12:24:07 +0100

    added context, a few routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
+import { Routes, Route } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
 
 function App() {
- 
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Routes> 
+            <Route index path="/" element={WelcomePage} />            
+            </Routes>
+            <Footer />
+        </>
+    );
 }
 
-export default App
+export default App;
+
+ 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import {UserProvider} from './contexts/User.jsx'
+
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+  <UserProvider>
+     <App />
+     </UserProvider>
+  </BrowserRouter>
+);
